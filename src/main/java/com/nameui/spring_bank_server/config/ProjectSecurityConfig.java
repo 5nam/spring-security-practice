e740c34afa985ba8350b5ac7865fa606ce5a80ba
5nam <ohnam90@gmail.com>
2024-08-26 17:18:39 +0900

    InMemoryUserDetailsManager 로 여러 사용자 생성하기

--- src/main/java/com/nameui/spring_bank_server/config/ProjectSecurityConfig.java
+++ src/main/java/com/nameui/spring_bank_server/config/ProjectSecurityConfig.java
@@ -3,6 +3,9 @@ package com.nameui.spring_bank_server.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -19,5 +22,27 @@ public class ProjectSecurityConfig {
                 .formLogin(withDefaults())
                 .httpBasic(withDefaults());
         return http.build();
+    }
+
+    /**
+     * User 클래스를 통해 withDefaultPasswordEncoder 메서드 호출
+     *
+     * @return
+     */
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager() {
+        UserDetails admin = User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("12345")
+                .authorities("admin")
+                .build();
+
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("12345")
+                .authorities("read")
+                .build();
+
+        return new InMemoryUserDetailsManager(admin, user);
     }
 }
